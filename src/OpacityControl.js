c237f48e28ebb7a4a9b7a27ffa0d56580ec97e65
nonzeronucleus <gitkraken@ianplumb.org>
2019-01-21 22:29:45 +0000

    Improves opacity slider

--- src/OpacityControl.js
+++ src/OpacityControl.js
@@ -14,7 +14,7 @@ const Wrapper = styled.div`
 const Slider = styled.input`
   // opacity:1;
   // -webkit-appearance: none;  /* Override default CSS styles */
-  // appearance: none;
+  appearance: none;
   // width: 100%; /* Full-width */
   // height: 5px; /* Specified height */
   // // background: #d3d3d3; /* Grey background */
@@ -23,20 +23,35 @@ const Slider = styled.input`
   // opacity: 1.0; /* Set transparency (for mouse-over effects on hover) */
   // -webkit-transition: .2s; /* 0.2 seconds transition on hover */
   // transition: opacity .2s;
+  padding:10px;
+  border:10px;
 
   :hover {
     // opacity: 1; /* Fully shown on mouse-over */
   }
 
-  // ::-webkit-slider-thumb {
+  ::-webkit-slider-thumb {
   //   -webkit-appearance: none; /* Override default look */
   //   appearance: none;
-  //   width: 25px; /* Set a specific slider handle width */
-  //   height: 25px; /* Slider handle height */
+    width: 25px; /* Set a specific slider handle width */
+    height: 25px; /* Slider handle height */
   //   background: #4CAF50; /* Green background */
   //   border-radius:50%;
   //   cursor: pointer; /* Cursor on hover */
-  // }
+  }
+
+
+  ::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #ffffff;
+    cursor: pointer;
+    margin-top: -14px; /* You need to specify a margin in Chrome, but in Firefox and IE it is automatic */
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d; /* Add cool effects to your sliders! */
+  }
 
   // ::-moz-range-thumb {
   //   width: 25px; /* Set a specific slider handle width */
@@ -44,6 +59,17 @@ const Slider = styled.input`
   //   background: #4CAF50; /* Green background */
   //   cursor: pointer; /* Cursor on hover */
   // }
+
+  ::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 8.4px;
+    cursor: pointer;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    background: #3071a9;
+    border-radius: 1.3px;
+    border: 0.2px solid #010101
+  }
+
 }
 
 `;
